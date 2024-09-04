c860d61c509c03b1438ca4c4d96cb26402a7392d
unknown <andile.goba@younglings.africa>
2024-09-04 11:58:53 +0200

    contactjsx update file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { FaInstagram } from 'react-icons/fa';
 import { FaFacebook } from 'react-icons/fa';
-import { FaWhatsapp } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
 import { FaVoicemail } from 'react-icons/fa';
 import { CONTACT } from '../constants';
 
@@ -10,22 +10,10 @@ const Contact = () => {
     return(
         <div className="border-b border-neutral-900 pb-20">
             <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
-            <div className="text-center tracking-tighter flex flex-wrap lg:justify-center">
-                <div className-="w-full lg:w-1/4">
-                    <div className="my-4">
-                        <FaInstagram />
-                        <FaFacebook />
-                        <FaWhatsapp />
-                        <FaVoicemail />
-                    </div>
-                </div>
-                <div className="w-full max-w-xl lg:w-3/4">
-                    <p className="my-1"><a className="text-decoration-none" href="https://www.instagram.com/ndilisa.thesongstress/">ndilisa.thesongstress</a></p>
-                    <p className="my-1"><a className="text-decoration-none" href="https://www.facebook.com/profile.php?id=61564416838280">ndilisa the songstress</a></p>
-                    <p className="my-1">(+27)-076 638 8256</p>
-                    <a className="text-decoration-none" href="bookings4ndilisa@outlook.com">bookings4ndilisa@outlook.com</a> 
-                </div>
-                
+            <div className="text-center tracking-tighter flex items-center gap-4 lg:justify-center">
+                <a className="text-decoration-none" href="https://www.instagram.com/ndilisa.thesongstress/"><FaInstagram /></a>
+                <a className="text-decoration-none" href="https://www.facebook.com/profile.php?id=61564416838280"><FaFacebook /></a>
+                <a className="text-decoration-none" href="bookings4ndilisa@outlook.com"><FaVoicemail /></a> 
             </div>
         </div>
     )
