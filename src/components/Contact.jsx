4fca128ae42d045fb160ddce2a25a5f8bd21e472
unknown <andile.goba@younglings.africa>
2024-09-05 21:34:49 +0200

    contact file update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { FaInstagram } from 'react-icons/fa';
 import { FaFacebook } from 'react-icons/fa';;
 import { AiFillMail } from "react-icons/ai";
+import { SiLinkedin } from "react-icons/si";
+import { AiFillTikTok } from "react-icons/ai";
 import { CONTACT } from '../constants';
 
 const Contact = () => {
@@ -13,6 +15,8 @@ const Contact = () => {
                 <a className="text-decoration-none" href="https://www.instagram.com/ndilisa.thesongstress/"><FaInstagram /></a>
                 <a className="text-decoration-none" href="https://www.facebook.com/profile.php?id=61564416838280"><FaFacebook /></a>
                 <a className="text-decoration-none" href="https://outlook.live.com/mail/0/?ui=en-US&rs=US&auth=1"><AiFillMail /></a> 
+                <a className="text-decoration-none" href="https://www.linkedin.com/in/ndilisa-goba-886037327?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><SiLinkedin /></a>
+                <a className="text-decoration-none" href="https://www.tiktok.com/@ndilisa.goba?_t=8pTNl1gDZwL&_r=1"><AiFillTikTok /></a>  
             </div>
         </div>
     )
